cb391efd46572d476ab364c6e7bda663663eb590
naidelii <naidelii@qq.com>
2024-07-19 10:11:51 +0800

    build：增加swagger模块

---
+++ mall-common/mall-common-swagger/src/main/java/com/mall/commom/swagger/config/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+package com.mall.commom.swagger.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author naidelii
+ * Swagger文档配置
+ */
+@Configuration
+@EnableKnife4j
+public class SwaggerConfiguration {
+
+
+    @Bean
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 添加摘要信息
+     *
+     * @return 摘要信息
+     */
+    private ApiInfo apiInfo() {
+        // 用ApiInfoBuilder进行定制
+        return new ApiInfoBuilder()
+                // 设置标题
+                .title("mall-guli接口文档")
+                // 描述
+                .description("接口文档")
+                // 作者信息
+                .contact(new Contact("Naidelii", "https://www.naidelii.top/", "naidelii@qq.com"))
+                // 版本
+                .version("1.0")
+                .build();
+    }
+
+
+}
